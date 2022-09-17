c6c670779ddff022a0e26b94093157b3d6f2aa60
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-09-17 17:50:18 -0300

    Mapeamento das entidades Estado e Municipio

--- localidade-api/src/main/java/com/jeanbarcellos/localidade/entities/Estado.java
+++ localidade-api/src/main/java/com/jeanbarcellos/localidade/entities/Estado.java
@@ -1,19 +1,34 @@
 package com.jeanbarcellos.localidade.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "estado")
 public class Estado {
 
+    @Id
     @EqualsAndHashCode.Include
+    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "sigla", nullable = false, length = 2)
     private String sigla;
 
+    @Column(name = "nome", nullable = false)
     private String nome;
 
 }
