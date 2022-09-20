e7e6970ad15c6a024b083ba6779578b4fd355a00
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-09-19 23:22:47 -0300

    Ajustes no Mapper

--- localidade-api/src/main/java/com/jeanbarcellos/localidade/mapper/LocalidadeMapper.java
+++ localidade-api/src/main/java/com/jeanbarcellos/localidade/mapper/LocalidadeMapper.java
@@ -1,7 +1,11 @@
 package com.jeanbarcellos.localidade.mapper;
 
+import java.util.function.LongFunction;
+
+import com.jeanbarcellos.localidade.dtos.EnderecoRequest;
 import com.jeanbarcellos.localidade.dtos.ibge.MunicipioResponse;
 import com.jeanbarcellos.localidade.dtos.ibge.UFResponse;
+import com.jeanbarcellos.localidade.entities.Endereco;
 import com.jeanbarcellos.localidade.entities.Estado;
 import com.jeanbarcellos.localidade.entities.Municipio;
 
@@ -10,19 +14,31 @@ public class LocalidadeMapper {
     private LocalidadeMapper() {
     }
 
-    public static Estado toEstado(UFResponse response) {
+    public static Estado toEstado(UFResponse request) {
         return Estado.builder()
-                .id(response.getId())
-                .nome(response.getNome())
-                .sigla(response.getSigla())
+                .id(request.getId())
+                .nome(request.getNome())
+                .sigla(request.getSigla())
                 .build();
     }
 
-    public static Municipio toMunicipio(MunicipioResponse response) {
+    public static Municipio toMunicipio(MunicipioResponse request) {
         return Municipio.builder()
-                .id(response.getId())
-                .nome(response.getNome())
+                .id(request.getId())
+                .nome(request.getNome())
                 .build();
+    }
+
+    public static Endereco copyProperties(Endereco endereco, EnderecoRequest request,
+            LongFunction<Municipio> municipioRepositoryFind) {
+        return endereco
+                .setId(request.getId())
+                .setCep(request.getCep())
+                .setMunicipio(municipioRepositoryFind.apply(request.getMunicipioId()))
+                .setBairro(request.getBairro())
+                .setLogradouro(request.getLogradouro())
+                .setNumero(request.getNumero())
+                .setComplemento(request.getComplemento());
     }
 
 }
